333fba33bac16f89d7e71451d940aab10babf922
jenna0704 <99992708+jenna0704@users.noreply.github.com>
2022-07-05 12:53:38 +0000

    updated Array2DPractice.java

--- programming/3/Array2DPractice.java
+++ programming/3/Array2DPractice.java
@@ -27,5 +27,48 @@ public class Array2DPractice
     }
     return board;
   }
-  
+
+//printBoard
+public static void printBoard(char[][] board)
+{
+   for (int rows = 0; rows < board.length; rows++)
+   {
+       for (int cols = 0; cols < board[0].length; cols++)
+       {
+           System.out.print(board[rows][cols]);
+       }
+
+       System.out.println("");
+   }
+}
+
+
+//setRow
+public static void setRow(char[][] board, int row, char value)
+{
+    for (int c = 0; c < board[0].length; c++)
+    {
+        board[row][c] = value;
+    }
+}
+
+
+//copyBoard
+public static char[][] copyBoard(char[][] original)
+{
+    char[][] duplicateBoard = new char[original.length][original[0].length];
+
+    for (int rows = 0; rows < original.length; rows++)
+    {
+        for (int cols = 0; cols < original[0].length; cols++)
+        {
+            duplicateBoard[rows][cols] = original[rows][cols];
+        }
+    }
+
+    printBoard(duplicateBoard);
+
+    return duplicateBoard;
+
+}
 }
