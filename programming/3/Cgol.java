4400776854cbcebc8527adad903898330b1679c2
jenna0704 <99992708+jenna0704@users.noreply.github.com>
2022-07-06 16:01:25 +0000

    added Cgol.java

--- programming/3/Cgol.java
+++ programming/3/Cgol.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 /**
  * Conway's Game of Life Team AreWeSentientYet?
- * First Last
- * collaborators: First Last, First Last
+ * Jenna Lin
+ * collaborators: Mike Randazzo, Adam Prado, Cassara
  */
 
 /**
@@ -25,79 +25,138 @@ import java.util.*;
 
 public class Cgol
 {
+    static char dead = '-';
+    static char alive = 'X';
 
-  //create, initialize, and return  empty board (all cells dead)
-  public static char[][] createNewBoard( int rows, int cols )
-  {
+    public static void main( String[] args )
+    {
+        char[][] board;
+        board = createNewBoard(5,5);
 
-  }
+        //breathe life into some cells:
+        setCell(board, 0, 0, 'X');
+        setCell(board, 0, 1, 'X');
+        setCell(board, 1, 0, 'X');
 
+        System.out.println("Gen X:");
+        printBoard(board);
+        System.out.println("--------------------------\n\n");
 
-  //print the board to the terminal
-  public static void printBoard( char[][] board )
-  {
+        board = generateNextBoard(board);
 
-  }
+        System.out.println("Gen X+1:");
+        printBoard(board);
+        System.out.println("--------------------------\n\n");
 
-
-  //set cell (r,c) to val
-  public static void setCell( char[][] board, int r, int c, char val )
-  {
-
-  }
-
-
-  //return number of living neigbours of board[r][c]
-  public static int countNeighbours( char[][] board, int r, int c )
-  {
-
-  }
-
-
-  /**
-     precond: given a board and a cell
-     postcond: return next generation cell state based on CGOL rules
-     (alive 'X', dead ' ')
-  */
-  public static char getNextGenCell( char[][] board,int r, int c )
-  {
-
-  }
-
-
-  //generate and return a new board representing next generation
-  public static char[][] generateNextBoard( char[][] board )
-  {
-
-  }
+    }
+   
+ 
+    //create, initialize, and return  empty board (all cells dead)
+    public static char[][] createNewBoard(int rows, int cols)
+    {
+        char[][] board = new char[rows][cols];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                board[i][j] = dead;
+            }
+        }
+        return board;
+    }
 
 
-  public static void main( String[] args )
-  {
-    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    char[][] board;
-    board = createNewBoard(25,25);
+    //print the board to the terminal
+    public static void printBoard(char[][] board)
+    {
+        for (int i = 0; i < board.length; i++)
+        {
+            for (int j = 0; j < board[0].length; j++)
+            {
+                System.out.print(board[i][j] + " ");
+            }
 
-    //breathe life into some cells:
-    setCell(board, 0, 0, 'X');
-    setCell(board, 0, 1, 'X');
-    setCell(board, 1, 0, 'X');
+            System.out.println();
+        }
+    }
 
-    // TASK:
-    // Once your initial version is running,
-    // try out different starting configurations of living cells...
-    // (Feel free to comment out the above three lines.)
 
-    System.out.println("Gen X:");
-    printBoard(board);
-    System.out.println("--------------------------\n\n");
+    //set cell (r,c) to val
+    public static void setCell( char[][] board, int r, int c, char val )
+    {
+        board[r][c] = val;
+    }
 
-    board = generateNextBoard(board);
 
-    System.out.println("Gen X+1:");
-    printBoard(board);
-    System.out.println("--------------------------\n\n");
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-  }//end main()
+    //return number of living neigbours of board[r][c]
+    public static int countNeighbours( char[][] board, int r, int c )
+    {
+        int numOfNeighbours = 0;
 
-}//end class
+        for (int i = Math.max(0, r - 1); i <= Math.min(r + 1, board.length - 1); i++)
+        {
+            for (int j = Math.max(0, c - 1); j <= Math.min(c + 1, board[0].length - 1); j++)
+            {
+                if (!(i == r && j == c) && board[i][j] == alive)
+                {          
+                    numOfNeighbours++;                   
+                }
+            }
+        }       
+            return numOfNeighbours;
+    }
+
+
+    /**
+        precond: given a board and a cell
+        postcond: return next generation cell state based on CGOL rules
+        (alive 'X', dead ' ')
+    */
+    public static char getNextGenCell( char[][] board,int r, int c )
+    {
+        int counts = countNeighbours(board, r, c);
+
+        if(board[r][c] == alive)
+        {
+            if (counts == 2 || counts == 3)
+            {
+                return alive;
+            }
+            else 
+            {
+                return dead;
+            }
+        }
+        else
+        {
+            if (counts == 3)
+            {
+                return alive;
+            }
+            else 
+            {
+                return dead;
+            }
+        }
+    }
+
+
+    //generate and return a new board representing next generation
+    public static char[][] generateNextBoard( char[][] board )
+    {
+        char[][] nextBoard = new char[board.length][board[0].length];
+
+        for(int i = 0; i < board.length; i++)
+        {
+            for (int j = 0; j < board[i].length; j++)
+            {
+                nextBoard[i][j] = getNextGenCell(board, i, j);
+            }
+        }
+
+        return nextBoard;
+    }
+
+
+
+}
